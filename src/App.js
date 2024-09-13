5a6998efb9a26564abdb2c17fadbf8ddd53753ea
SFCC Trainer <co.sfcc@cloudodyssey.co.uk>
2024-09-13 16:40:24 +0200

    Committing files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
         <h2>Render Salesforce LWC in React</h2>
         <iframe
-            src="https://spde12-dev-ed.lightning.force.com/external-lwc.html"
+            src="https://lwc-app-react.netlify.app/app-index.html"
             width="600"
             height="400"
             title="Salesforce LWC"
