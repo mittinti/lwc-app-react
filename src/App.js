9570a62d0846da9a39cc3e35fd225148357e17b6
SFCC Trainer <co.sfcc@cloudodyssey.co.uk>
2024-09-13 16:27:33 +0200

    Committing files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h2>Render Salesforce LWC in React</h2>
+        <iframe
+            src="https://spde12-dev-ed.lightning.force.com/external-lwc.html"
+            width="600"
+            height="400"
+            title="Salesforce LWC"
+        ></iframe>
     </div>
   );
 }
