e534971993b64a6c912bd68e12e043305a36e3a7
SFCC Trainer <co.sfcc@cloudodyssey.co.uk>
2024-09-16 14:44:37 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-
+import React from 'react';
+import LWCComponent from './components/LWC';
+ 
 function App() {
   return (
-    <div>
-        <h2>Render Salesforce LWC in React</h2>
-        <iframe
-            src="https://lwc-app-react.netlify.app/app-index.html"
-            width="600"
-            height="400"
-            title="Salesforce LWC"
-        ></iframe>
-    </div>
+<div className="App">
+<header className="App-header">
+<h1>React App with Salesforce Lightning Component</h1>
+</header>
+  <LWCComponent />
+</div>
   );
 }
-
+ 
 export default App;
---
+++ src/components/LWC.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react';
+ 
+const LWCComponent = () => {
+  const lightningDivRef = useRef(null);
+ 
+  useEffect(() => {
+    // Load the Lightning Out JavaScript library
+    const script = document.createElement('script');
+    script.src = 'https://spde12-dev-ed.lightning.force.com/lightning/lightning.out.js'; // Replace with your Salesforce domain
+    script.onload = () => {
+      // Initialize Lightning Out
+      $Lightning.use(
+        'c:LightningOutApp', // Replace with your Aura app name
+        () => {
+          // Create and render the Lightning component
+          $Lightning.createComponent(
+            'c:LwcWrapper', // Replace with your Lightning component name
+            {}, // Attributes to pass to the component, if any
+            lightningDivRef.current, // DOM element where the component will render
+            (cmp) => {
+              console.log('Salesforce Component Rendered', cmp);
+            }
+          );
+        },
+        'https://spde12-dev-ed.lightning.force.com',
+        '00Df4000001TR9x!ARAAQNy6BWWGUYhGCkPVvUySrAKJ8pvECP47izCt0vsYslLNpkQadVX3gjFHAbiMVLrl09C91mDiPn5pTtQC8Dp61mUzhrVz' // Replace with your Salesforce domain
+      );
+    };
+    document.body.appendChild(script);
+ 
+    // Clean up script on component unmount
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+ 
+  return (
+<div>
+<h1>Salesforce Lightning Component</h1>
+      {/* This div is where the Salesforce component will be rendered */}
+<div ref={lightningDivRef}></div>
+</div>
+  );
+};
+ 
+export default LWCComponent;
